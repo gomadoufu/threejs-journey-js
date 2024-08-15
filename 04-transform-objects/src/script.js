55e996d4bfb80865153687b453704f12c708878e
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-08-15 09:18:40 +0900

    04

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -1,3 +1,13 @@
+/**
+ * 04. Transform Objects
+ * - オブジェクトを動かしたり、スケールさせたり、回転させる方法
+ *     - position, scale, rotation
+ * - あるオブジェクトを別のオブジェクトの方向に向かせる方法
+ *     - lookAt
+ * - 複数のオブジェクトをグループ化する方法
+ *     - Group
+ */
+
 import * as THREE from 'three'
 
 // Canvas
@@ -7,12 +17,50 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube1.position.x = - 1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube2.position.x = 0
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
+// transformations
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+// mesh.scale.x = 2
+// mesh.scale.y = 0.25
+// mesh.scale.z = 0.5
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
 
 /**
  * Sizes
@@ -27,6 +75,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// lookAtメソッド
+// camera.lookAt(new THREE.Vector3(0, -1, 0))
 scene.add(camera)
 
 /**
